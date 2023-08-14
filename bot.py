53eb217eee42680f14fa0943e4a63468f811c880
noahtheprogrammer <weavernoah11@gmail.com>
2023-08-14 07:37:37 -0400

    Basic test command

--- bot.py
+++ bot.py
@@ -6,10 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    await message.channel.send("Test.")
 
 client.run(TOKEN)
---
+++ commands.py
@@ -0,0 +1,17 @@
+import json
+
+from solders.pubkey import Pubkey
+from solana.rpc.api import Client
+from solana.rpc.types import TokenAccountOpts
+
+client = Client("https://api.mainnet-beta.solana.com/")
+payout_address = Pubkey.from_string("3D3whwTbRYzr2b6yY8hXE6EVGRJVGYyGC7jFrTxHVhbK")
+
+def token_balance(address: str) -> float:
+    response = client.get_token_accounts_by_owner_json_parsed(payout_address, TokenAccountOpts(mint = Pubkey.from_string(address))).to_json()
+    json_response = json.loads(response)
+    balance = json_response["result"]["value"][0]["account"]["data"]["parsed"]["info"]["tokenAmount"]["uiAmount"]
+    return(balance)
+
+# EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v (USDC)
+# Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB (USDT)
